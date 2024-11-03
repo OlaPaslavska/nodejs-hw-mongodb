0348bd140ae3760dbcbb4a24915de0f69dc3e4c0
Olya Paslavska <157712045+OlaPaslavska@users.noreply.github.com>
2024-11-03 20:27:00 +0100

    hw2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -6,10 +6,10 @@ dotenv.config();
 
 export const initMongoConnection = async () => {
   if (
-    !process.env.MONGODB_USER ||
-    !process.env.MONGODB_PASSWORD ||
-    !process.env.MONGODB_URL ||
-    !process.env.MONGODB_DB
+    process.env.MONGODB_USER ||
+    process.env.MONGODB_PASSWORD ||
+    process.env.MONGODB_URL ||
+    process.env.MONGODB_DB
   ) {
     console.error(
       'MongoDB connection configuration is missing in environment variables.',
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-const bootstrap = async () => {
-  await initMongoConnection();
-  setupServer();
+const startApp = async () => {
+  try {
+    await initMongoConnection();
+
+    setupServer();
+  } catch (error) {
+    console.error('Error during app initialization:', error);
+  }
 };
 
-bootstrap();
+startApp();

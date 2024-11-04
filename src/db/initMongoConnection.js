0b20832f1ff3c71e7b2f38e7299cf7fff1961773
Olya Paslavska <157712045+OlaPaslavska@users.noreply.github.com>
2024-11-04 14:58:53 +0100

    hw2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -5,17 +5,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const initMongoConnection = async () => {
-  if (
-    process.env.MONGODB_USER ||
-    process.env.MONGODB_PASSWORD ||
-    process.env.MONGODB_URL ||
-    process.env.MONGODB_DB
-  ) {
-    console.error(
-      'MongoDB connection configuration is missing in environment variables.',
-    );
-    process.exit(1);
-  }
   try {
     await mongoose.connect(
       `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority&appName=cluster0`,

f25924d3be0596dc6a90f650ec7d6f7a2bb9ee55
Olya Paslavska <157712045+OlaPaslavska@users.noreply.github.com>
2024-11-16 13:53:47 +0100

    changes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 
-import { env } from './utils/env.js';
+import env from './utils/env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
@@ -10,6 +10,7 @@ const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
 
+  app.use(express.json());
   app.use(cors());
 
   app.use(
